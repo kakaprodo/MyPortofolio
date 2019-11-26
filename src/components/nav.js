17443c051df6f70d4413dcd06da41adf26236e9b
kakaprodo <promskakaprodo45@gmail.com>
2019-11-26 16:29:40 +0100

    ft(welcome):the page is ready

---
+++ src/components/nav.js
@@ -0,0 +1,19 @@
+import React from 'react';
+
+class Nav extends React.Component
+{
+  render() {
+  	return (
+       <div>
+          <ul>
+          	 <li> Home </li>
+          	 <li> About </li>
+          	 <li> Skills </li>
+          	 <li> Project </li>
+          	 <li> Motivation </li>
+          	 
+          </ul>
+       </div>
+  	);
+  }
+}
