25ef24fb99e7c3467ef6ac74f6e325aad90fd196
kakaprodo <promskakaprodo45@gmail.com>
2019-11-27 17:25:46 +0100

    Updates

---
+++ precache-manifest.6d5ca1d97ed3be310e040b9afe1962ad.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "669a47b9720dbdd45ffa817aed2db8d4",
+    "url": "/MyPortofolio/index.html"
+  },
+  {
+    "revision": "cdbd6cec3ff08fdc2472",
+    "url": "/MyPortofolio/static/css/main.34de6062.chunk.css"
+  },
+  {
+    "revision": "679b045ed70a90bb8ead",
+    "url": "/MyPortofolio/static/js/2.f2ca3761.chunk.js"
+  },
+  {
+    "revision": "cdbd6cec3ff08fdc2472",
+    "url": "/MyPortofolio/static/js/main.5a674d73.chunk.js"
+  },
+  {
+    "revision": "888cfa083b132bf845fd",
+    "url": "/MyPortofolio/static/js/runtime-main.7a95f41f.js"
+  }
+]);
