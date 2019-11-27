07f769ff623d53dd671c161ec474eb87bdf7f179
kakaprodo <promskakaprodo45@gmail.com>
2019-11-27 17:11:43 +0100

    bg(routes):changing BrowserRouter

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { BrowserRouter as Router } from "react-router-dom"
+import { HashRouter as Router } from "react-router-dom"
 import BasicRoutes from './router/index';
 import Nav from './components/nav';
 
