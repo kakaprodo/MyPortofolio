d821c99522fdde4b21a0d06d8fc3346995a9ac74
kakaprodo <promskakaprodo45@gmail.com>
2019-11-27 17:39:51 +0100

    bg(images):fixing display

--- src/components/welcome.js
+++ src/components/welcome.js
@@ -15,7 +15,7 @@ export default class Welcome extends React.Component{
            
              <div className="col xl8 l9 m10 s12 app-opacity push-xl2 push-l2 push-m1 center wlm-presentation">
                <div className="center wlm_img">
-                  <img src="/img/me.jpg" alt="" />
+                  <img src="../../img/me.jpg" alt="" />
                </div>
                <p className="title-known"> { me.surname } { me.lastname }.</p>
                <p className="title-known"> { me.title }. </p>
